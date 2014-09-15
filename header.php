3cb79088bcc5241bf590fec62ebfb506b812760a
clemencet <clemence.m.thomas@gmail.com>
2014-09-15 15:38:16 +0200

    php
    Passage au PHP

---
+++ header.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title><?php echo "title" ?></title>
+	<meta name="description" content="<?php echo "$meta_description" ?>">
+	<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Habibi' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Courgette' rel='stylesheet' type='text/css'>
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!-- Leave those next 4 lines if you care about users using IE8 -->
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>	
+	<div class="container bg-white">
+		<div class="row"> <!--Row1-->
+			<header>
+				<div class="row">
+					<h1>Mon Blog</h1>
+				</div>
+				<nav class="navbar navbar-default" role="navigation">
+					<div class="container-fluid">
+						<div class="navbar-header">
+					    	<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					    		<span class="sr-only">Toggle navigation</span>
+					        	<span class="icon-bar"></span>
+					       		<span class="icon-bar"></span>
+					        	<span class="icon-bar"></span>
+					    	</button>
+					    </div><!--fermture navbar-header-->
+					    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						    <ul class="nav navbar-nav">
+							    <a href="blog.html" class="bold underline">Blog</a>
+								<a href="#" class="bold underline">Fiction</a>
+								<a href="#" class="bold underline">Activité pro</a>
+								<a href="#" class="bold underline">A propos</a>
+								<a href="#" class="bold underline"><i class="fa fa-envelope-o"></i></a>
+						    </ul>
+						</div> <!--fermeture navbar collapse-->
+					</div><!-- fermeture container-fluid -->
+				</nav>
+			</header>
+		</div> <!--fermeturerow1-->
+			<div class="row"> <!--Row2-->
+				<div class="col-md-10">
